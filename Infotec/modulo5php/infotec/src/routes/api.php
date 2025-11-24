33b057e92519d434ed0e16be6d0ebc22cf012776
Melanie Contreras <dsidev2@semarnat.gob.mx>
2025-11-24 12:02:51 -0600

    practica 5b

--- Infotec/modulo5php/infotec/src/routes/api.php
+++ Infotec/modulo5php/infotec/src/routes/api.php
@@ -7,36 +7,36 @@ use App\Http\Controllers\EventoController;
 use App\Http\Controllers\PonentesController;
 use App\Http\Controllers\AsistentesController;
 
-//----Rutas para el recurso Evento.
-// Recuperar todos los eventos
+//Rutas públicas
 Route::get('/eventos', [EventoController::class, 'index']);
-// Almacenar un evento nuevo
-Route::post('/eventos', [EventoController::class, 'store']);
-// Recuperar un evento específico
 Route::get('/eventos/{id}', [EventoController::class, 'show']);
-// Actualizar un evento específico
-Route::put('/eventos/{evento}', [EventoController::class, 'update']);
-// Eliminar un evento específico
-Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
-// ----Rutas para el recurso ponentes.
-// Recuperar todos los ponentes
 Route::get('/ponentes', [PonentesController::class, 'index']);
-// Almacenar un ponente nuevo
-Route::post('/ponentes', [PonentesController::class, 'store']);
-// Recuperar un ponente específico
 Route::get('/ponentes/{id}', [PonentesController::class, 'show']);
-// Actualizar un ponente específico
-Route::put('/ponentes/{ponente}', [PonentesController::class, 'update']);
-// Eliminar un ponente específico
-Route::delete('/ponentes/{id}', [PonentesController::class, 'destroy']);
-// ----Rutas para el recurso asistentes.
-// Recuperar todos los asistentes
-Route::get('/asistentes', [AsistentesController::class, 'index']);
-// Almacenar un asistente nuevo
-Route::post('/asistentes', [AsistentesController::class, 'store']);
-// Recuperar un asistente específico
-Route::get('/asistentes/{id}', [AsistentesController::class, 'show']);
-// Actualizar un asistente específico
-Route::put('/asistentes/{asistente}', [AsistentesController::class, 'update']);
-// Eliminar un asistente específico
-Route::delete('/asistentes/{id}', [AsistentesController::class, 'destroy']);
+
+//Rutas privadas
+Route::middleware('auth:api')->group(function () {
+    // Almacenar un evento nuevo
+    Route::post('/eventos', [EventoController::class, 'store']);
+    // Actualizar un evento específico
+    Route::put('/eventos/{evento}', [EventoController::class, 'update']);
+    // Eliminar un evento específico
+    Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+    // ----Rutas para el recurso ponentes.
+    // Almacenar un ponente nuevo
+    Route::post('/ponentes', [PonentesController::class, 'store']);
+    // Actualizar un ponente específico
+    Route::put('/ponentes/{ponente}', [PonentesController::class, 'update']);
+    // Eliminar un ponente específico
+    Route::delete('/ponentes/{id}', [PonentesController::class, 'destroy']);
+    // ----Rutas para el recurso asistentes.
+    // Recuperar todos los asistentes
+    Route::get('/asistentes', [AsistentesController::class, 'index']);
+    // Almacenar un asistente nuevo
+    Route::post('/asistentes', [AsistentesController::class, 'store']);
+    // Recuperar un asistente específico
+    Route::get('/asistentes/{id}', [AsistentesController::class, 'show']);
+    // Actualizar un asistente específico
+    Route::put('/asistentes/{asistente}', [AsistentesController::class, 'update']);
+    // Eliminar un asistente específico
+    Route::delete('/asistentes/{id}', [AsistentesController::class, 'destroy']);
+});
